eac883be0d8e4d50da09bfc2d69163b8c4d9b1ff
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-09-25 15:26:56 +0200

    Landing Page

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,86 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import Colors from "./../constants/Colors";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        backgroundColor: Colors.WHITE,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Image
+        source={require("./../assets/images/landing.png")}
+        style={{
+          width: "100%",
+          height: 300,
+          marginTop: 80,
+        }}
+      />
+
+      <View
+        style={{
+          padding: 25,
+          backgroundColor: Colors.PRIMARY,
+          height: "100%",
+          borderTopLeftRadius: 35,
+          borderTopRightRadius: 35,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "bold",
+            textAlign: "center",
+            color: Colors.WHITE,
+          }}
+        >
+          Welcome to StudyBuddy
+        </Text>
+        <Text
+          style={{
+            fontSize: 20,
+            color: Colors.WHITE,
+            marginTop: 20,
+            textAlign: "center",
+          }}
+        >
+          Transform your ideas into engaging educational content, effortlessly
+          with AI!📚🤖
+        </Text>
+        <View style={styles.button}>
+          <Text style={[styles.buttonText, { color: Colors.PRIMARY }]}>
+            Get Started
+          </Text>
+        </View>
+        <View
+          style={[
+            styles.button,
+            {
+              backgroundColor: Colors.PRIMARY,
+              borderWidth: 1,
+              borderColor: Colors.WHITE,
+            },
+          ]}
+        >
+          <Text style={[styles.buttonText, { color: Colors.WHITE }]}>
+            Already have an Account?
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 15,
+    backgroundColor: Colors.WHITE,
+    marginTop: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    textAlign: "center",
+    fontSize: 18,
+  },
+});
