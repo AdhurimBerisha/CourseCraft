14528005c2507ed81bcf08eef307fca9ccbc1645
AdhurimBerisha <adhurimberisha07@gmail.com>
2025-09-28 01:38:30 +0200

    Removed duplicate

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -51,7 +51,6 @@ export default function Home() {
         <CourseProgress courseList={courseList}/>
         <PracticeSection />
         <CourseList courseList={courseList} /> 
-        <CourseList courseList={courseList} /> 
       </View> 
         }
     </View>
